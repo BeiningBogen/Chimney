3dd28f38cea9df810e59c0176e2539e12f477eb7
Håkon Bogen <bogen@beiningbogen.no>
2020-03-17 10:50:55 +0100

    Fix code layout

--- Sources/Chimney/Chimney.swift
+++ Sources/Chimney/Chimney.swift
@@ -154,29 +154,17 @@ extension Requestable {
         return config
     }
 }
-extension PathComponentsProvider {
-    /// If a request path has the base URL as first path component, set that as the base URL for the request and remove it from path components
-    func baseURLAndPathComponents() -> (String?, [String]) {
-        if let firstPathComponent = pathComponents.path.first, firstPathComponent.contains("https://") || firstPathComponent.contains("http://") {
-            var components = pathComponents.path
-            components.removeFirst()
-            return (firstPathComponent, components)
-        }
-        return (environment.configuration?.baseURL.absoluteString, pathComponents.path)
-    }
-}
+
 
 extension Requestable {
     internal static func requestData(path: Path, parameters: Parameter?, sessionConfig: URLSessionConfiguration? = nil, completion: @escaping ((Result<Data, RequestableError>) -> Void)) {
 
         let (foundBaseURL, pathComponents) = path.baseURLAndPathComponents()
-        
         guard let baseURL = foundBaseURL else {
             print("No base URL set or given")
             return
         }
-        
-        
+
         let auth: [String: String]?
         var urlComponents: URLComponents
 
@@ -237,6 +225,18 @@ extension Requestable {
             } catch {
                 completion(.failure(.underlying(error: error)))
             }
+    }
+}
+
+extension PathComponentsProvider {
+    /// If a request path has the base URL as first path component, set that as the base URL for the request and remove it from path components
+    func baseURLAndPathComponents() -> (String?, [String]) {
+        if let firstPathComponent = pathComponents.path.first, firstPathComponent.contains("https://") || firstPathComponent.contains("http://") {
+            var components = pathComponents.path
+            components.removeFirst()
+            return (firstPathComponent, components)
+        }
+        return (environment.configuration?.baseURL.absoluteString, pathComponents.path)
     }
 }
 
